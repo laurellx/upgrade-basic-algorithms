522247a6d346d50c20142e9657db4427e54384e6
Laura <laurellx@MacBook-Pro-2.local>
2022-11-28 16:16:57 +0100

    iteración3

---
+++ operadores.js
@@ -0,0 +1,22 @@
+let a = 10;
+let b = 5;
+console.log(a * b);
+
+a = 10;
+b = 2;
+console.log(a / b);
+
+a = 15;
+b = 9;
+console.log(a % b);
+
+
+let y = 10;
+let z = 5
+x = y + z;
+console.log(x);
+
+x = y * z;
+console.log(x);
+
+
