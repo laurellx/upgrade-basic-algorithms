3b4b4da34b50f2275e8ed5cb20093452d529cf93
Laura <laurellx@MacBook-Pro-2.local>
2023-01-11 18:12:57 +0100

    iteration#3

---
+++ iteration#3.js
@@ -0,0 +1,26 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let a = 10;
+let b = 5;
+console.log(a * b);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+a = 10;
+b = 2;
+console.log(a / b);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+a = 15;
+b = 9;
+console.log(a % b);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15,
+// teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+x = y + z;
+console.log(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+x = y * z;
+console.log(x);
